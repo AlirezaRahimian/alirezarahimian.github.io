d53beb5f93ebac3de13727d021c2e8ad7d359b5f
Mc.Alireza Rahimian <alireza.rahimian91@mac.com>
2026-08-05 06:50:48 +0330

    test: modernize and polish the Sentry test example
    update test/index.html to include React 18, ReactDOM, and Sentry CDNs, add modern CSS styling, rewrite test.js to use React 18 createRoot API, add proper Sentry initialization configuration, and fix trailing newline

--- test/test.js
+++ test/test.js
@@ -1,13 +1,28 @@
-import * as Sentry from '@sentry/react';
-// Add this button component to your app to test Sentry's error tracking
+const { useState } = React;
+
+Sentry.init({
+  dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 function ErrorButton() {
   return (
-    <button
-      onClick={() => {
-        throw new Error('This is your first error!');
-      }}
-    >
-      Break the world
-    </button>
+    React.createElement('div', { className: 'container' },
+      React.createElement('h1', null, 'Sentry Error Test'),
+      React.createElement('button', {
+        onClick: () => {
+          throw new Error('This is your first error!');
+        }
+      }, 'Break the world')
+    )
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(ErrorButton));
